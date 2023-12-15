480aa440b22e805212ea439a30e12e75c7fdd7d4
yofp1937 <yofp1937@naver.com>
2023-12-15 15:29:56 +0900

    Initial commit

---
+++ src/main/java/com/Shop/ShoppingmallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shop/ShoppingmallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
